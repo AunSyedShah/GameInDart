9648f142ee84f05bd4215d5a7e54347865e53466
Aun Syed Shah <aunsyedsyed@gmail.com>
2020-10-05 22:16:58 +0500

    even function added

--- main.dart
+++ main.dart
@@ -1,4 +1,13 @@
+bool isEven(int x) {
+  if (x % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 main() {
   print("Guess Number Game");
   print("Added by Aun Syed Shah");
+  print(isEven(6));
 }
